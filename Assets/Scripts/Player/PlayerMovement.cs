adc0704f7242f84fc7619c30e1b321f8dadc8411
kindred008 <talia.preston123@gmail.com>
2024-04-02 14:08:59 +0100

    Created and added sprites for Player, Platform and Broken Platform

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,23 +5,34 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int moveSpeed = 5;
 
     private PlayerInput playerInput;
     private Rigidbody2D playerRb;
+    private SpriteRenderer playerSprite;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         playerRb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         var moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        
+
+        if (moveInput.x > 0)
+        {
+            playerSprite.flipX = false;
+        } else if (moveInput.x < 0)
+        {
+            playerSprite.flipX= true;
+        }
+
         playerRb.velocity = new Vector2(moveInput.x * moveSpeed, playerRb.velocity.y);
     }
 }
