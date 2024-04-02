7aae436102a3a50dd88fa0ebe220b008675b745b
kindred008 <talia.preston123@gmail.com>
2024-04-02 14:28:34 +0100

    Objects can be destroyed when jumped on

--- Assets/Scripts/Player/PlayerPhysics.cs
+++ Assets/Scripts/Player/PlayerPhysics.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -22,13 +23,24 @@ public class PlayerPhysics : MonoBehaviour
         var halfSize = (playerCollider.size.y / 2) * transform.localScale.y;
         var offset = new Vector3(halfSize, 0);
 
-        if (Physics2D.Raycast(transform.position, Vector2.down, halfSize + 0.05f) || 
-            Physics2D.Raycast(transform.position + offset, Vector2.down, halfSize + 0.05f) || 
-            Physics2D.Raycast(transform.position - offset, Vector2.down, halfSize + 0.05f))
+        RaycastHit2D leftHit = Physics2D.Raycast(transform.position - offset, Vector2.down, halfSize + 0.05f);
+        RaycastHit2D middleHit = Physics2D.Raycast(transform.position, Vector2.down, halfSize + 0.05f);
+        RaycastHit2D rightHit = Physics2D.Raycast(transform.position + offset, Vector2.down, halfSize + 0.05f);
+
+        var raycastHitArray = new RaycastHit2D[] {leftHit, middleHit, rightHit};
+        var raycastHitArrayCollided = raycastHitArray.Where(x => x.collider != null).ToArray();
+
+        if (raycastHitArrayCollided.Length > 0)
         {
             if (playerRb.velocity.y <= 0)
             {
                 playerRb.velocity = new Vector2(playerRb.velocity.x, bounceStrength);
+            }
+
+            var destructableHit = raycastHitArrayCollided.FirstOrDefault(x => x.collider.GetComponent<Destructable>() != null);
+            if (!destructableHit.Equals(default(RaycastHit2D)))
+            {
+                destructableHit.collider.GetComponent<Destructable>().Damage();
             }
         }
     }
